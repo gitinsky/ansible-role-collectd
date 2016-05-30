ce24a5552512439f0a19ddd03725153481eebeec
Alex Chistyakov <alexclear@gmail.com>
2016-05-30 20:21:25 +0300

    Introduce collectd_suffix

--- templates/collectd/collectd-eye.rb
+++ templates/collectd/collectd-eye.rb
@@ -1,12 +1,12 @@
-Eye.application 'collectd' do
+Eye.application 'collectd{% if collectd_suffix|default("") != "" %}-{{ collectd_suffix }}{% endif %}' do
   working_dir '/opt/collectd'
-  stdall '/var/log/eye/collectd-stdall.log' # stdout,err logs for processes by default
+  stdall '/var/log/eye/collectd-stdall{% if collectd_suffix|default("") != "" %}-{{ collectd_suffix }}{% endif %}.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
 
-  process :collectd do
-    pid_file '/var/run/collectd-eye.pid'
-    start_command '/opt/collectd/sbin/collectd -f'
+  process :collectd{% if collectd_suffix|default("") != "" %}_{{ collectd_suffix }}{% endif %} do
+    pid_file '/var/run/collectd-eye{% if collectd_suffix|default("") != "" %}-{{ collectd_suffix }}{% endif %}.pid'
+    start_command '/opt/collectd/sbin/collectd -f -C /opt/collectd/etc/collectd{% if collectd_suffix|default("") != "" %}-{{ collectd_suffix }}{% endif %}.conf'
 
     daemonize true
     start_timeout 10.seconds
